4274f255fb54140a77459fde23f229af57d4624e
xielongwang <984814106@qq.com>
2019-03-03 22:23:17 +0800

    maven pom 单独管理

---
+++ spring-cloud-saas-example/spring-cloud-saas-common-base/src/main/java/com/siaron/saas/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.siaron.saas.util;
+
+/**
+ * @author xielongwang
+ * @create 2019-03-039:27 PM
+ * @email xielong.wang@nvr-china.com
+ * @description
+ */
+public class DateUtil {
+
+
+}
--- spring-cloud-saas-example/spring-cloud-saas-user-center/README.md
+++
@@ -1,6 +0,0 @@
-# spring-cloud-saas-user-center
-
-## 表
-- saas_user 用户表
-
-## 各产品认证 + 集中式认证
